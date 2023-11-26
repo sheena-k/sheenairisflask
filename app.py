ea1099833f76be98c526700116d2c72159208346
sheena-k <141423563+sheena-k@users.noreply.github.com>
2023-11-26 09:11:10 +0000

    5

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+# load the model
+file = pickle.load(open("savemodel.pickle","rb"))
+
+@app.route("/")
+def home():
+    result =""
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST","GET"])
+def predict():
+    Sepal_Length = float(request.form["sepal_length"])
+    Sepal_Width = float(request.form["sepal_width"])
+    Petal_Length = float(request.form["petal_length"])
+    Petal_Width = float(request.form["petal_width"])
+    result = model.predict([[Sepal_Length,Sepal_Width,Petal_Length,Petal_Width]])[0]
+    return render_template("index.html", **locals())
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df= pd.read_excel("iris_python.xlsx")
+
+classifications = df["Classification"].value_counts()
+
+df["Classification"].unique()
+
+y = df["Classification"]
+x = df.drop(['Classification'],axis=1)
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(x,y, test_size = 0.2, random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+
+model = lr.fit(x_train,y_train)
+
+y_predictions = model.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+
+y_predictions
+
+print("accuracy =",accuracy_score(y_test,y_predictions))
+
+import pickle
+
+filename="savemodel.pickle"
+
+with open(filename,"wb") as file:
+  pickle.dump(model,file)
+
+
+
+
+
